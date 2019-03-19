22816cc65da73701879e45cc46222446d1cac3a6
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-03-19 01:30:25 -0700

    Fix component.mk include/src directories

--- component.mk
+++ component.mk
@@ -1,6 +1,6 @@
 INC_DIRS += $(ir_i2s_ROOT)
 
-# ir_i2s_INC_DIR = $(ir_i2s_ROOT)
-# ir_i2s_SRC_DIR = $(ir_i2s_ROOT)
+ir_i2s_INC_DIR = $(ir_i2s_ROOT)
+ir_i2s_SRC_DIR = $(ir_i2s_ROOT)
 
 $(eval $(call component_compile_rules,ir_i2s))
