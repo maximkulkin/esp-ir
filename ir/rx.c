56233d28d2a2a2596d162fe5babfbca440c44fe8
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-04-02 03:27:51 -0700

    Fix problem with using timer in interrupt

--- ir/rx.c
+++ ir/rx.c
@@ -14,6 +14,7 @@ typedef enum {
     ir_rx_state_idle,
     ir_rx_state_mark,
     ir_rx_state_space,
+    ir_rx_state_overflow,
 } ir_rx_state_t;
 
 
@@ -38,14 +39,18 @@ static ir_rx_context_t ir_rx_context;
 
 
 static void ir_rx_timeout(void *arg) {
-    if (ir_rx_context.buffer_pos) {
+    if ((ir_rx_context.state == ir_rx_state_idle) ||
+        (sdk_system_get_time() - ir_rx_context.last_time < ir_rx_context.timeout))
+        return;
 
-      int16_t *pulses = malloc(sizeof(int16_t) * (ir_rx_context.buffer_pos + 1));
-      for (int i=0; i < ir_rx_context.buffer_pos; i++)
-          pulses[i] = ir_rx_context.buffer[i];
-      pulses[ir_rx_context.buffer_pos] = 0;
 
-      xQueueSendToBack(ir_rx_context.receive_queue, &pulses, 10);
+    if (ir_rx_context.buffer_pos && (ir_rx_context.state != ir_rx_state_overflow)) {
+        int16_t *pulses = malloc(sizeof(int16_t) * (ir_rx_context.buffer_pos + 1));
+        for (int i=0; i < ir_rx_context.buffer_pos; i++)
+            pulses[i] = ir_rx_context.buffer[i];
+        pulses[ir_rx_context.buffer_pos] = 0;
+
+        xQueueSendToBack(ir_rx_context.receive_queue, &pulses, 10);
     }
 
     ir_rx_context.state = ir_rx_state_idle;
@@ -54,8 +59,6 @@ static void ir_rx_timeout(void *arg) {
 
 
 static void IRAM ir_rx_interrupt_handler(uint8_t gpio_num) {
-    sdk_os_timer_disarm(&ir_rx_context.timeout_timer);
-
     uint32_t now = sdk_system_get_time();
 
     switch (ir_rx_context.state) {
@@ -73,11 +76,17 @@ static void IRAM ir_rx_interrupt_handler(uint8_t gpio_num) {
             ir_rx_context.buffer[ir_rx_context.buffer_pos++] = -us;
             break;
         }
+
+        case ir_rx_state_overflow:
+            break;
     }
 
-    ir_rx_context.state = !gpio_read(ir_rx_context.gpio) ? ir_rx_state_mark : ir_rx_state_space;
+    if (ir_rx_context.buffer_pos >= ir_rx_context.buffer_size) {
+        ir_rx_context.state = ir_rx_state_overflow;
+    } else {
+        ir_rx_context.state = !gpio_read(ir_rx_context.gpio) ? ir_rx_state_mark : ir_rx_state_space;
+    }
 
-    sdk_os_timer_arm(&ir_rx_context.timeout_timer, ir_rx_context.timeout, 0);
     ir_rx_context.last_time = now;
 }
 
@@ -88,18 +97,18 @@ void ir_rx_init(uint8_t gpio, uint16_t rx_buffer_size) {
     ir_rx_context.buffer_size = rx_buffer_size;
     ir_rx_context.buffer_pos = 0;
 
-    gpio_enable(ir_rx_context.gpio, GPIO_INPUT);
-    gpio_set_interrupt(ir_rx_context.gpio, GPIO_INTTYPE_EDGE_ANY, ir_rx_interrupt_handler);
-
     ir_rx_context.state = ir_rx_state_idle;
-    ir_rx_context.timeout = 100;
-    sdk_os_timer_disarm(&ir_rx_context.timeout_timer);
+    ir_rx_context.timeout = 5000;  // 5 ms
     sdk_os_timer_setfn(&ir_rx_context.timeout_timer, ir_rx_timeout, NULL);
+    sdk_os_timer_arm(&ir_rx_context.timeout_timer, 200, 1);
 
     ir_rx_context.receive_queue = xQueueCreate(10, sizeof(int16_t*));
     if (!ir_rx_context.receive_queue) {
         printf("Failed to create IR receive queue\n");
     }
+
+    gpio_enable(ir_rx_context.gpio, GPIO_INPUT);
+    gpio_set_interrupt(ir_rx_context.gpio, GPIO_INTTYPE_EDGE_ANY, ir_rx_interrupt_handler);
 }
 
 
